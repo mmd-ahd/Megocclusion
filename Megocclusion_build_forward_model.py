7f533d60e85d6cf8c3a0d5d10f6d79a0fca8e36d
Mohammad Ahadzadeh <78875270+mmd-ahd@users.noreply.github.com>
2025-09-15 05:30:09 -0700

    Add files via upload

---
+++ Megocclusion_build_forward_model.py
@@ -0,0 +1,37 @@
+import os
+import mne
+
+dataset_dir = 'Megocclusion'
+output_dir = 'fwd'
+os.makedirs(dataset_dir, output_dir, exist_ok=True)
+fwd_fname = os.path.join(output_dir, 'fsaverage-meg-ico4-fwd.fif')
+
+sample_raw_fname = 'subj04NN_sess01-0_tsss.fif'
+
+if not os.path.exists(sample_raw_fname):
+    raise FileNotFoundError(
+        f"The file '{sample_raw_fname}' was not found. "
+    )
+
+subjects_dir = mne.datasets.sample.data_path() / 'subjects'
+os.environ['SUBJECTS_DIR'] = str(subjects_dir)
+
+src = mne.setup_source_space(subject='fsaverage', spacing='ico4',
+                             subjects_dir=subjects_dir, add_dist=False)
+
+model = mne.make_bem_model(subject='fsaverage', subjects_dir=subjects_dir)
+bem = mne.make_bem_solution(model)
+
+sample_raw = mne.io.read_raw_fif(sample_raw_fname, preload=False, on_split_missing='ignore')
+info = mne.pick_info(sample_raw.info, mne.pick_types(sample_raw.info, meg=True, eeg=False))
+info['bads'] = []
+sample_raw.close()
+
+trans = os.path.join(str(subjects_dir), 'fsaverage', 'bem', 'fsaverage-trans.fif')
+
+fwd = mne.make_forward_solution(info, trans=trans, src=src, bem=bem,
+                                meg=True, eeg=False, n_jobs=-1)
+
+mne.write_forward_solution(fwd_fname, fwd, overwrite=True)
+
+print(f"\nForward model computed and saved to:\n{os.path.abspath(fwd_fname)}")
