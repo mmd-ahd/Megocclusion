1d1be4edb682601d82b63df02dccd04c3a336eed
Mohammad Ahadzadeh <78875270+mmd-ahd@users.noreply.github.com>
2025-10-02 23:42:24 +0330

    Update Megocclusion_inverse_operators.py

--- Megocclusion_inverse_operators.py
+++ Megocclusion_inverse_operators.py
@@ -8,6 +8,10 @@ os.makedirs(output_dir, exist_ok=True)
 
 sub_ids = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 13, 14, 15]
 occlusion_levels = ['0', '60', '80']
+subject_ranks = {
+    1: 70, 2: 72, 3: 71, 4: 73, 5: 59, 6: 70, 7: 67, 8: 69,
+    9: 70, 10: 70, 12: 65, 13: 68, 14: 71, 15: 66
+}
 
 fwd_fname = os.path.join(dataset_dir, 'fwd', 'fsaverage-meg-oct6-fwd.fif')
 fwd = mne.read_forward_solution(fwd_fname)
@@ -34,7 +38,7 @@ for sub in sub_ids:
     
     all_epochs = mne.concatenate_epochs(epochs_list)
 
-    noise_cov = mne.compute_covariance(all_epochs, tmax=0.0, method='auto')
+    noise_cov = mne.compute_covariance(all_epochs, tmax=0.0, method='auto', rank={'meg': subject_ranks[sub]})
     
     inv_op = make_inverse_operator(all_epochs.info, fwd, noise_cov, loose=0.2, depth=None)
     
